2e643223de66c497df7b9e8fe0d5eaaa71893bfa
Daniel <unchirito@gmail.com>
2022-09-19 10:57:01 -0500

    final 0.1

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const parser = require('body-parser');
 const app = express();
 const port = 4000;
-const datoRoutes = require("./routes/dato");
 const mongoose = require("mongoose");
-const { application } = require("express");
 require('dotenv').config();
 var cors = require('cors');
+const router = express.Router();
+const datoSchema = require("./dato");
 
        
 app.use(cors())
 app.use(parser.urlencoded({ extended: false }));
 app.use(parser.json());
 
-app.use(datoRoutes);
+app.use(router);
 app.use(express.json());
 
 mongoose
@@ -25,3 +25,46 @@ console.clear();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
+
+
+router.get("/", (req,res)=>{
+  res.send("Holo");
+});
+
+
+//Agregar un documento
+router.post("/p", (req, res) => {
+  const dato = datoSchema(req.body);
+ dato
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//Listar todos los documentos
+router.get("/g", (req, res) => {
+ datoSchema.find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//Buscar por id
+router.get("/dato/:id", (req, res) => {
+  const { id } = req.params;
+ datoSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar por id
+router.delete("/dato/:id", (req, res) => {
+  const { id } = req.params;
+ datoSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
